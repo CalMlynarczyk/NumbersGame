aea0f820508a3bfc12318d74b5dc9e786e3c7785
Calvin Mlynarczyk <cal.mlynarczyk@gmail.com>
2012-11-28 23:32:36 -0500

    Renamed data context class for convention purposes

--- NumbersGame/Models/NumbersGameContext.cs
+++ NumbersGame/Models/NumbersGameContext.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace NumbersGame.Models
 {
-    public class NumbersGameEntities : DbContext
+    public class NumbersGameContext : DbContext
     {
         public DbSet<Game> Games { get; set; }
         public DbSet<Move> Moves { get; set; }
